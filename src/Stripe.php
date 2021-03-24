2c3d613e4a71c07ba95d2a8ac9d468b71aa26af1
Joel Butcher <joelbutcher@users.noreply.github.com>
2021-03-24 06:12:33 +0000

    Update Stripe.php

--- src/Stripe.php
+++ src/Stripe.php
@@ -6,10 +6,11 @@ class Stripe implements PaymentProcessorInterface
 {
     /**
      * Process a receipt via Stripe.
+     *
      * @param  \App\Receipt  $receipt
      * @return \App\Receipt
      */
-    public function process(Receipt $receipt)
+    public function process(Receipt &$receipt)
     {
         // Logic
 
