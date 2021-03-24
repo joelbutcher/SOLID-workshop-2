379170d8b386b90c217f7954f9da4675c97b9f73
Joel Butcher <joelbutcher@users.noreply.github.com>
2021-03-24 06:08:45 +0000

    Create Stripe.php

---
+++ src/Stripe.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class Stripe implements PaymentProcessorInterface
+{
+    /**
+     * Process a receipt via Stripe.
+     * @param  \App\Receipt  $receipt
+     * @return \App\Receipt
+     */
+    public function process(Receipt $receipt)
+    {
+        // Logic
+        
+        return $receipt;
+    }
+}
